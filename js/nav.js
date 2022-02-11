1ba72fe423cf6c6189065f999ae317509ca3d6ff
kwonch <rnjsskfen153@naver.com>
2022-02-11 12:31:47 +0900

    2022-02-11
    메뉴를 클릭하면 자연스럽게 이동효과 넣음
    푸터까지 완성

--- js/nav.js
+++ js/nav.js
@@ -8,3 +8,14 @@ $(function(){
 
 
 });
+
+$(function(){
+    //클래스 .menu_btn인 a 태그를 클릭할시 발생되게 하는 이벤트
+    
+    $(".menu_btn").click(function(event){
+
+        $("html,body").animate({scrollTop:$(this.hash).offset().top},500)
+    });
+  //$(this.hash) a태그에 있는 해쉬 값으로 스크롤링 되며 이동
+  //500은 스크롤 이동
+});
